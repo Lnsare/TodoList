d54584297382f284750c4afba9ded3b870ff453d
Lnsare <43454637+Lnsare@users.noreply.github.com>
2019-02-06 14:48:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var taskInput = document.getElementById("addTask");
+
+var createNewTasElemet =  function addNewTask() {
+    alert("1");
+    }
+document.getElementById('button').onclick = createNewTasElemet;
+    
